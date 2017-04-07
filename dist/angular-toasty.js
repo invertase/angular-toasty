935fdd10717573541f273cf17e0db82072ae2209
Joe Tinoco <joseph.tinoco@rbc.com>
2017-04-07 14:50:49 -0400

    Build with $interval instead of $timeout

--- dist/angular-toasty.js
+++ dist/angular-toasty.js
@@ -31,7 +31,7 @@
  */
 
 angular.module('angular-toasty', []);
-angular.module('angular-toasty').directive('toasty', ['toasty', '$timeout', '$sce', function(toasty, $timeout, $sce) {
+angular.module('angular-toasty').directive('toasty', ['toasty', '$interval', '$sce', function(toasty, $interval, $sce) {
 	return {
 		replace: true,
 		restrict: 'EA',
@@ -105,7 +105,7 @@ angular.module('angular-toasty').directive('toasty', ['toasty', '$timeout', '$sc
 			// Custom setTimeout function for specific
 			// setTimeouts on individual toasts
 			function setTimeout(toasty, time) {
-				toasty.timeout = $timeout(function() {
+				toasty.timeout = $interval(function() {
 					clear(toasty.id);
 				}, time);
 			}
